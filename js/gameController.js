97df981f03b4b20f4a22b6893154987215066ebe
Will Carter <whcarter3@gmail.com>
2015-11-04 22:42:27 -0800

    commented code for process

--- js/gameController.js
+++ js/gameController.js
@@ -6,8 +6,8 @@
 		function gameController($http){
 			var self = this;
 
+			//View Accessible variables
 			self.getRandomIMDBinfo 	= getRandomIMDBinfo;
-			// self.randomIMDBnumber 		= null;
 			self.randomTitle	= null;
 			self.randomActors	= null;
 			self.thinkingGif	= {'display': 'none'};
@@ -15,13 +15,16 @@
 			function getRandomIMDBinfo (){
 				self.thinkingGif = {'display': 'block'};
 
+				//Random ID number from 1000000 - 9999999
 				var randomIMDBnumber = Math.floor(Math.random() * (9999999 - 1000001) + 1000000);
+				//URL for API call + random ID #
 				var url = 'http://www.omdbapi.com/?i=tt' + randomIMDBnumber + '&type=movie';
 
 				function splitValues(str){
 					str.split(',');
 				};
 
+				//API Call
 				$http.get(url)
 					.success(function(data, status, headers, config){
 						console.log(url);
@@ -30,16 +33,7 @@
 						var genre = data.Genre;
 						var genreCheck = false;
 						if(genre){
-							// var genreArray	= splitValues(data.Genre);
-							// var genre = null;
-							// function documentaryCheck (arr){
-							// 	for (var i = 0; i < arr.length; i++) {
-							// 		if(arr[i] == "Documentary" || arr[i] == " Documentary"){
-							// 			genre = true;
-							// 		}
-							// 	};
-							// }
-							// documentaryCheck(genreArray);
+							//means actual response
 							console.log('genre found');
 
 							if(genre.indexOf('Documentary') > -1){
@@ -48,7 +42,8 @@
 
 						}
 
-						if(data.Response == 'False' || data.Metascore == "N/A" || metascore < 30 || data.Language !== "English" || genre){
+						//If response does not pass filter, run function again
+						if(data.Response == 'False' || data.Metascore == "N/A" || metascore < 30 || data.Language !== "English" || genreCheck){
 							getRandomIMDBinfo();
 						}
 						else{
