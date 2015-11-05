20a6cdd226f829dd4063ca270dd442bf702c4d56
Will Carter <whcarter3@gmail.com>
2015-11-04 22:30:25 -0800

    got functionality of filters

--- js/gameController.js
+++ js/gameController.js
@@ -3,18 +3,65 @@
 		.module('app')
 		.controller('gameController', gameController);
 
-		function gameController(){
+		function gameController($http){
 			var self = this;
 
-			self.getRandomIMDBnumber 	= getRandomIMDBnumber;
-			self.randomIMDBnumber 		= null;
+			self.getRandomIMDBinfo 	= getRandomIMDBinfo;
+			// self.randomIMDBnumber 		= null;
+			self.randomTitle	= null;
+			self.randomActors	= null;
+			self.thinkingGif	= {'display': 'none'};
 
-			// function getRandomIMDBnumber(){
-			// 	return Math.floor(Math.random * (9999999 - 1000000) + 1000000);
-			// };
+			function getRandomIMDBinfo (){
+				self.thinkingGif = {'display': 'block'};
 
-			function getRandomIMDBnumber (){
-				self.randomIMDBnumber = Math.floor(Math.random() * (9999999 - 1000001) + 1000000);
+				var randomIMDBnumber = Math.floor(Math.random() * (9999999 - 1000001) + 1000000);
+				var url = 'http://www.omdbapi.com/?i=tt' + randomIMDBnumber + '&type=movie';
+
+				function splitValues(str){
+					str.split(',');
+				};
+
+				$http.get(url)
+					.success(function(data, status, headers, config){
+						console.log(url);
+
+						var metascore = parseInt(data.Metascore);
+						var genre = data.Genre;
+						var genreCheck = false;
+						if(genre){
+							// var genreArray	= splitValues(data.Genre);
+							// var genre = null;
+							// function documentaryCheck (arr){
+							// 	for (var i = 0; i < arr.length; i++) {
+							// 		if(arr[i] == "Documentary" || arr[i] == " Documentary"){
+							// 			genre = true;
+							// 		}
+							// 	};
+							// }
+							// documentaryCheck(genreArray);
+							console.log('genre found');
+
+							if(genre.indexOf('Documentary') > -1){
+								genreCheck = true;
+							}
+
+						}
+
+						if(data.Response == 'False' || data.Metascore == "N/A" || metascore < 30 || data.Language !== "English" || genre){
+							getRandomIMDBinfo();
+						}
+						else{
+							var actorsArray = splitValues(data.Actors);
+
+							self.randomActor = actorsArray[3];
+							self.randomTitle = data.Title;
+							self.thinkingGif = {'display': 'none'};
+						}
+					})
+					.error(function(data, status, headers, config){
+						self.randomIMDBtitle = "An error has occurred";
+					})
 			}
 		}
 })();
